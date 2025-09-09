dfc5548d515612321fc2eb4322269a5e12cbb7bb
davithekid <byjdrk.27@gmail.com>
2025-09-09 15:31:58 -0300

    feat: graficos corretos

--- front-end/app/src/components/blocks/separator/separator.jsx
+++ front-end/app/src/components/blocks/separator/separator.jsx
@@ -1,30 +1,36 @@
 import { Separator } from "@/components/ui/separator"
 import { CardDemo } from "../card/card"
-import { ChartPieLabel, Component } from "@/components/graficos/Barra"
+import { ChartPieLabel } from "@/components/graficos/PieLabel"
+import { ChartBarLabel } from "@/components/graficos/BarLabel"
+import { ChartLineLinear } from "@/components/graficos/LineChart"
 
 
 export function SeparatorDemo() {
     return (
         <div>
-            <div className="space-y-1">
-                <h4 className="text-sm lg:text-xl leading-none font-bold">Dashboard</h4>
-                <p className="text-muted-foreground text-sm">
-                    Acompanhe seus componentes principais.
-                </p>
-                <div className="grid grid-cols-3 py-4">
-                    <CardDemo />
+            <section>
+                <div className="space-y-1">
+                    <h4 className="text-sm lg:text-xl leading-none font-bold">Dashboard</h4>
+                    <p className="text-muted-foreground text-sm">
+                        Acompanhe seus componentes principais.
+                    </p>
+                    <div className="grid grid-cols-3 py-4">
+                        <CardDemo />
+                    </div>
                 </div>
-            </div>
-            <section className="mx-auto container ">
+            </section>
+
+            <section className="mx-auto container py-8 ">
                 <div className="space-y-1 ">
                     <h4 className="text-sm lg:text-xl leading-none  flex justify-center font-bold">Acompanhe seu rendimento</h4>
                     <p className="text-muted-foreground text-sm flex justify-center">
                         Gráfico correpondente a sua entrada, saida e metas.
                     </p>
                 </div>
-                <div className="flex py-8">
-                    <Component/>
-                    <Component/>
+                <div className="flex justify-center gap-12 py-8">
+                    <ChartBarLabel />
+                    <ChartPieLabel />
+                    <ChartLineLinear />
                 </div>
             </section>
             <Separator className="my-4" />
---
+++ front-end/app/src/components/graficos/LineChart.jsx
@@ -0,0 +1,88 @@
+"use client"
+
+import { TrendingUp } from "lucide-react"
+import { LineChart, Line, XAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+
+export const description = "A clean linear line chart"
+
+const chartData = [
+  { month: "January", desktop: 186 },
+  { month: "February", desktop: 305 },
+  { month: "March", desktop: 237 },
+  { month: "April", desktop: 73 },
+  { month: "May", desktop: 209 },
+  { month: "June", desktop: 214 },
+]
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "var(--chart-1)",
+  },
+}
+
+export function ChartLineLinear() {
+  return (
+    <Card className="border-none shadow-none bg-transparent">
+      <CardHeader>
+        <CardTitle>Line Chart - Linear</CardTitle>
+        <CardDescription>January - June 2024</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig} className="h-60">
+          <LineChart
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+              top: 10,
+              bottom: 10,
+            }}
+          >
+            {/* removido o grid para deixar limpo */}
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Line
+              dataKey="desktop"
+              type="linear"
+              stroke="var(--color-desktop)"
+              strokeWidth={2}
+              dot={false}
+            />
+          </LineChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col items-start gap-2 text-sm">
+        <div className="flex gap-2 leading-none font-medium">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="text-muted-foreground leading-none">
+          Showing total visitors for the last 6 months
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
